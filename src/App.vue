7550617fd25a1f0bb2110a4fcaed0db9fb4e8b9e
DmitryGlinskiy <glinskiy.dmitry@gmail.com>
2019-04-15 05:29:29 +0300

    UI prototype

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <template lang="pug">
 
   #app
-    img(alt="Vue logo" src="./assets/logo.png")
-    HelloWorld(msg="Welcome to Your Vue.js App")
+    #title Warships. Evasive
+    #settings ⚙️
+    #alerts You miss. Yarr!
+
+    #fieldMy.field-grid(v-html="this.getMyField")
+    .fieldExtraLeft Evade!
+
+    #fieldTheir.field-grid(v-html="this.getTheirField")
+    .fieldExtraRight Attack!
+    
+    // img(alt="Vue logo" src="./assets/logo.png")
+    // HelloWorld(msg="Welcome to Your Vue.js App")
 
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+
+  computed: {
+    // mix the getters into computed with object spread operator
+    ...mapGetters([
+      'getMyField',
+      'getTheirField',
+    ])
   }
 }
 </script>
 
 <style lang="sass">
+@import 'sass/reset.sass';
 @import 'sass/main.sass';
 </style>
--- src/store.js
+++ src/store.js
@@ -5,7 +5,55 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    fieldMy: [
+      // aTP = allowedToPlace
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: false}, {aTP: false}, {aTP: false}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: false}, {aTP: false}, {aTP: false}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: false}, {aTP: false}, {aTP: false}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}]
+    ],
+    fieldTheir: [
+      // aTP = allowedToPlace
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}],
+      [{aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}, {aTP: true}]
+    ]
+  },
+  getters: {
+    // getField(player, [one / two]): state => {}
+    getMyField: state => {
+      let myFieldCells = '';
+      for (let i = 0; i < state.fieldMy.length; i++) {
+        for (let k = 0; k < state.fieldMy[i].length; k++) {
+          let allowed = state.fieldMy[i][k].aTP ? 'allowed' : 'forbidden'
+          myFieldCells += '<div class="row' + i + ' col' + k + ' ' + allowed + '">&nbsp;</div> ';
+        }
+      }
+      return myFieldCells;
+    },
+    getTheirField: state => {
+      let theirFieldCells = '';
+      for (let i = 0; i < state.fieldTheir.length; i++) {
+        for (let k = 0; k < state.fieldTheir[i].length; k++) {
+          let allowed = state.fieldTheir[i][k].aTP ? 'allowed' : 'forbidden'
+          theirFieldCells += '<div class="row' + i + ' col' + k + ' ' + allowed + '">&nbsp;</div> ';
+        }
+      }
+      return theirFieldCells;
+    }
   },
   mutations: {
 
